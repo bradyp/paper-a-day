318525cd6f03fe7e79edfd33549da1bd9e05d820
reflores <rfloresmeath@tamu.edu>
2012-11-20 22:56:56 -0600

    Updated hits for stability. Added non-converging(?) sparse implementation of hits, and the tests for the current version of hits.

--- hits.py
+++ hits.py
@@ -13,31 +13,33 @@ TEST_DOCS = [
         "id":1,
         "citations":  [5],
         "references": [3, 2],
-        "keywords": ["asdf"]
+        "keywords": ["asdf", "arbitrary", "latent direchlet"]
     },
     {
         "id":2,
-        "citations":  [],
-        "references": [1, 5],
-        "keywords": []
+        "citations":  [1, 5, 3],
+        "references": [],
+        "keywords": ["lda", "topic modelling", "most authoritative"]
     },
     {
         "id":3,
-        "citations":  [5],
-        "references": [1],
-        "keywords": []
+        "citations":  [5, 1],
+        "references": [2],
+        "keywords": [""]
     },
     {
         "id":5,
-        "citations":  [2],
-        "references": [3],
-        "keywords": []
+        "citations":  [],
+        "references": [3, 2, 1],
+        "keywords": ["pagerank", "the least authoritative"]
+    },
+    {
+        "id":314159,
+        "citations":  [57721],
+        "references": [271828],
+        "keywords": ["xyzzy"]
     }
     ]
-    
-
-from stemming import porter2
-
 
 def tokenize(text):
     """
@@ -58,37 +60,23 @@ class HITS(object):
     def __init__(self):
       self.index = collections.defaultdict(set)
       self.docs = {}
-
+      
     #run HITS over an iterator of dicts
     def run_hits(self, G, k = 20):
         #G is a set of docids
         for p in G:
           self.docs[p]['auth'] = 1  #self.docs[p]['auth'] is the authority score of the page p
           self.docs[p]['hub'] = 1 #self.docs[p]['hub'] is the hub score of the page p
-          
-        print self.docs
-           
+
         for step in range(0, k):  #run the algorithm for k steps
-          norm = 0.0
           for p in G:  #update all authority values first
-            self.docs[p]['oldauth'] = self.docs[p]['auth']
-            self.docs[p]['auth'] = sum(self.docs[q]['hub'] for q in self.docs[p]['citations'])
-            norm += math.pow(self.docs[p]['auth'], 2) #calculate the sum of the squared auth values to normalize
-          norm = math.sqrt(norm)
-          
-          for p in G:  #update the auth scores
-            self.docs[p]['auth'] = self.docs[p]['auth'] / norm  #normalize the auth values
-          
-          norm = 0.0
-          for p in G:  #then update all hub values
-            self.docs[p]['hub'] = sum(self.docs[r]['oldauth'] for r in self.docs[p]['references'])
-            norm += math.pow(self.docs[p]['hub'], 2) #calculate the sum of the squared hub values to normalize
-          norm = math.sqrt(norm)
+            self.docs[p]['auth'] = sum(self.docs[q]['hub'] for q in self.docs[p]['citations'] and G)
 
           for p in G:  #then update all hub values
-            self.docs[p]['hub'] = self.docs[p]['hub'] / norm   #normalize the hub values
-            
-            
+            self.docs[p]['hub'] = sum(self.docs[r]['auth'] for r in self.docs[p]['references'] and G)
+
+
+
     def index_docs(self, docs):
         for doc in docs:
           id = doc['id']
@@ -97,48 +85,67 @@ class HITS(object):
           tokens = set(tokenize(doc['keywords']))
           for token in tokens:
             self.index[token].add(id)
+            
+    def setup_baseset(self, query):
+        tokens = tokenize([query])
+        id_sets = [self.index[token] for token in tokens]
+        
+        if not id_sets or not all(id_sets):
+          print "No matches found"
+          return
+        ids = reduce(operator.__and__,id_sets)
+
+        matching_docs = [self.docs[id] for id in ids] #this is the root set
+        
+        doc_ids = [doc for doc in self.docs]
+        
+
+        #create set of only the docs that conform to the query topics or are directly connected to the query topics
+        base_set = set()  #set of doc ids
+        for doc in matching_docs:
+          #calculate the base set
+          base_set.add(doc['id']) #add the doc id to base_set
+          
+          #TODO is there a way to get around tossing the non-captured ACM cites/refs?
+          real_citations = set(doc['citations']).intersection(set(doc_ids))
+          base_set = base_set.union(real_citations) #add the citations to base_set
+          real_references = set(doc['references']).intersection(set(doc_ids))
+          base_set = base_set.union(real_references)  #add the references to base_set
+        return base_set
+
+
+def get_query(fromCL = True):    ##allowing for possible changes to this later
+    if fromCL:
+        return input("Enter query: ")
+
+def get_docs(useDefault = True):    ##pulling from test corpus for now
+    if useDefault:
+        return TEST_DOCS
 
 def main():
-    #FIXME snag the docs from wherever
-    docs = TEST_DOCS
+    #snag the docs from whereever
+    docs = get_docs()
 
     hits_obj = HITS()
 
     #make a set of all the stemmed keywords (tokens, more or less)
     hits_obj.index_docs(docs)
 
-    #FIXME run lda on the keywords
-    #FIXME be able to select multiple nums of topics
-    #mm = gensim.corpora.MmCorpus('')
-    #lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics = 4, update_every=0, passes=20)
+    #read queries
+    query = get_query()
+    
+    #TODO run lda on the new query
 
-    #read queries until read "/q"
-    while True:
-      query = input("Enter query or \"/q\" to quit: ")
-      if query == "/q":
-        return
-      #run lda on the new query
-      #doc_lda = lda[query]
+    base_set = hits_obj.setup_baseset(query)
+
+    hits_obj.run_hits(base_set, 5)
+    
+    ranked_docs = [hits_obj.docs[doc] for doc in hits_obj.docs if doc in base_set]
+    ranked_docs = sorted(ranked_docs, key=operator.itemgetter('auth'), reverse=True)
       
-      tokens = tokenize([query])
-      id_sets = [hits_obj.index[token] for token in tokens]
-      if not id_sets or not all(id_sets):
-          print []
-      ids = reduce(operator.__and__,id_sets)
-      
-      matching_docs = [hits_obj.docs[id] for id in ids] #this is the root set
-      
-      #FIXME create an iterator of only the docs that conform to the query topics or are directly connected to the query topics
-      base_set = set()  #set of doc ids
-      for doc in matching_docs:
-        #calculate the base set
-        base_set.add(doc['id']) #add the doc id to base_set
-        base_set = base_set.union(set(doc['citations'])) #add the citations to base_set
-        base_set = base_set.union(set(doc['references']))  #add the references to base_set
-      
-      hits_obj.run_hits(base_set, 5)
-      for doc in hits_obj.docs:
-        print(doc, hits_obj.docs[doc]['auth'], hits_obj.docs[doc]['hub'])
+    print "Top docs: "
+    for doc in ranked_docs:
+      print doc['id']
 
 if __name__=="__main__":
-    main()
+    main() 
