c0cc5d8e9734c42ffc36802c7e1eca486830891f
Skizzerz <skizzerz@gmail.com>
2012-12-04 08:04:00 -0600

    Fix html form and stylize a bit better

--- hits/sparsehits.py
+++ hits/sparsehits.py
@@ -150,15 +150,18 @@ def get_docs(useDefault = True):    ##pulling from test corpus for now
     if useDefault:
         server = couchdb.client.Server(url='https://vertex.skizzerz.net:6984/')
         db = server['papers']
-        return db.view('all/all')
-    return TEST_DOCS
+        result = db.view('all/all')
+        docs = []
+        for row in result:
+            docs.append(row.value)
+        return docs
 
 def main():
     #FIXME snag the docs from whereever
     docs = get_docs()
     print len(docs)
 
-    hits_obj = HITS()
+    hits_obj = sparseHITS()
 
     #index all the docs
     hits_obj.index_docs(docs)
